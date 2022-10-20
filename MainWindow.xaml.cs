90a7b9d0ca0c78700b7ed77d026e2af28a544d90
Luca Ionut-Catalin <ic.luca@outlook.com>
2022-10-20 21:15:16 +0200

    programmer website generator done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,60 @@ namespace SimpleWebsiteGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string? filename;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = @"D:\ITHS_Repositories";
+            if (dlg.ShowDialog() == true)
+                filename = dlg.FileName;
+
+            FileStream inputStream;
+            StreamReader reader;
+
+
+            try
+            {
+                inputStream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite);
+                reader = new StreamReader(inputStream);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Cannot open {filename}.html!" + ex);
+                return;
+            }
+            webText.Text = reader.ReadToEnd();
+            reader.Dispose();
+            inputStream.Close();
+        }
+
+        private void saveButton1_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream outputStream;
+            StreamWriter writer;
+
+            string path = @"D:\ITHS\Programmering med C# - projekter\SimpleWebsiteGenerator\programmer-" + saveName.Text + ".html";
+            try
+            {
+                outputStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(outputStream);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open WebsiteGenerator.html for writing" + ex);
+                return;
+            }
+
+            writer.Write(webText.Text);
+            writer.Close();
+            outputStream.Close();
+            MessageBox.Show("Saved!");
+
         }
     }
 }
